22fe7952c405431575555d29e9464e1fc25990ee
RahulChandrakarr <rrahulchandrakar1@gmail.com>
2025-10-31 22:35:27 +0530

    with dist file

---
+++ dist/_next/static/chunks/7f61e4d509fed611.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/e5ab006311ed32be.js",
+  "static/chunks/turbopack-f58c3cba37445f59.js"
+])
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
 
 export default nextConfig;
